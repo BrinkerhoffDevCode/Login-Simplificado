3162a17c83bd8945caf133ec052ee487afd29a72
BrinkerhoffDevCode <allyson_brinkerhoff@estudante.sesisenai.com.br>
2025-09-09 12:11:11 -0300

    update - database

--- login.php
+++ login.php
@@ -2,7 +2,7 @@
 // login.php
 
 // 1) Conexão
-$mysqli = new mysqli("localhost", "root", "", "login_db");
+$mysqli = new mysqli("localhost", "root", "root", "login_db");
 if ($mysqli->connect_errno) {
     die("Erro de conexão: " . $mysqli->connect_error);
 }
